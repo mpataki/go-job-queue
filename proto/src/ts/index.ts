afccb8f9cbd3ad3904b9f50c3fed6f2203f4f30a
Mat Pataki <matpataki@gmail.com>
2025-11-20 21:38:26 -0500

    fix: js proto package

--- proto/src/ts/index.ts
+++ proto/src/ts/index.ts
@@ -1,3 +1,3 @@
 // Re-export generated types
 export * from "../../gen/es/mpataki/go_job_queue/proto/job/v1/job_service_pb.js";
-export * from "../../gen/connect-es/mpataki/go_job_queue/proto/job/v1/job_service_connect.js";
+export { JobService as JobServiceConnect } from "../../gen/connect-es/mpataki/go_job_queue/proto/job/v1/job_service_connect.js";
