5ac97b7f9762e8b2d29d91056f758eec66fbfcb0
Mat Pataki <matpataki@gmail.com>
2025-11-20 21:58:18 -0500

    simplify js build setup some more

--- proto/src/ts/index.ts
+++
@@ -1,3 +0,0 @@
-// Re-export generated types
-export * from "../../gen/es/mpataki/go_job_queue/proto/job/v1/job_service_pb.js";
-export { JobService as JobServiceConnect } from "../../gen/connect-es/mpataki/go_job_queue/proto/job/v1/job_service_connect.js";
