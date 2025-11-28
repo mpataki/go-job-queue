30b21b52c0b50556c04a2c4b661a263f7f75d77c
Mat Pataki <matpataki@gmail.com>
2025-11-27 23:07:51 -0500

    feat: add get and cancel job cli commands

--- service/cmd/cli/main.go
+++ service/cmd/cli/main.go
@@ -23,11 +23,13 @@ func main() {
 	rootCmd.PersistentFlags().StringP("server", "s", "http://localhost:8080", "Job Server address")
 
 	rootCmd.AddCommand(newSubmitCommand())
+	rootCmd.AddCommand(newGetJobCommand())
+	rootCmd.AddCommand(newCancelJobCommand())
 	rootCmd.Execute()
 }
 
 func newSubmitCommand() *cobra.Command {
-	submitCmd := &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "submit",
 		Short: "Submit a job",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -55,11 +57,73 @@ func newSubmitCommand() *cobra.Command {
 		},
 	}
 
-	submitCmd.Flags().String("type", "", "Job type")
-	submitCmd.Flags().String("payload", "", "Job payload")
-	submitCmd.Flags().Int64("at", time.Now().UnixMilli(), "Execution time")
-	submitCmd.MarkFlagRequired("type")
-	submitCmd.MarkFlagRequired("payload")
+	cmd.Flags().String("type", "", "Job type")
+	cmd.Flags().String("payload", "", "Job payload")
+	cmd.Flags().Int64("at", time.Now().UnixMilli(), "Execution time")
+	cmd.MarkFlagRequired("type")
+	cmd.MarkFlagRequired("payload")
 
-	return submitCmd
+	return cmd
+}
+
+func newGetJobCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get",
+		Short: "Get a job",
+		Run: func(cmd *cobra.Command, args []string) {
+			serverAddr, _ := cmd.Root().PersistentFlags().GetString("server")
+
+			id, _ := cmd.Flags().GetString("id")
+
+			client := jobqueuev1connect.NewJobServiceClient(http.DefaultClient, serverAddr)
+
+			ctx := context.Background()
+
+			resp, err := client.GetJob(ctx, connect.NewRequest(&jobqueuev1.GetJobRequest{
+				Id: id,
+			}))
+			if err != nil {
+				log.Fatalf("Error fetching job from service: %v", err)
+			}
+
+			data, _ := json.MarshalIndent(resp.Msg, "", "  ")
+			fmt.Println(string(data))
+		},
+	}
+
+	cmd.Flags().String("id", "", "Job ID")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
+}
+
+func newCancelJobCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "cancel",
+		Short: "Cancel a job",
+		Run: func(cmd *cobra.Command, args []string) {
+			serverAddr, _ := cmd.Root().PersistentFlags().GetString("server")
+
+			id, _ := cmd.Flags().GetString("id")
+
+			client := jobqueuev1connect.NewJobServiceClient(http.DefaultClient, serverAddr)
+
+			ctx := context.Background()
+
+			resp, err := client.CancelJob(ctx, connect.NewRequest(&jobqueuev1.CancelJobRequest{
+				Id: id,
+			}))
+			if err != nil {
+				log.Fatalf("Error cancelling a job: %v", err)
+			}
+
+			data, _ := json.MarshalIndent(resp.Msg, "", "  ")
+			fmt.Println(string(data))
+		},
+	}
+
+	cmd.Flags().String("id", "", "Job ID")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
 }
